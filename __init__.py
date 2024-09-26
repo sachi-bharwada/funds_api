ae227c03f695c868113011b3b5a72f80e87b8a95
sachi-bharwada <sachi.bharwada@outlook.com>
2024-09-25 21:55:00 -0400

    intial files setup

---
+++ __init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import SQLAlchemy
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+database = os.getenv("DB_NAME")
+
+app = Flask(__name__)
+db = SQLAlchemy()
+
+app.config("SQLALCHEMY_DATABASE_URI") = "postgresql://f'{username}':f'{password}'@localhost:5432/f'{database}'"
+
+db.init_app(app)
